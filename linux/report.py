46d50a1c509bfd72c93c301c36971458dbeef4f0
Jake Liu <13583702+LittleJake@users.noreply.github.com>
2021-03-01 16:02:29 +0800

    update using psutil.

--- linux/report.py
+++ linux/report.py
@@ -1,66 +1,104 @@
-import redis, subprocess, time, json, hashlib, re, math
+import hashlib
+import json
+import math
+import re
+import redis
+import subprocess
+import time
+import requests
+import psutil
 
-HOST = ""
-PORT = ""
-PASSWORD = ""
-SALT = ""
+HOST = "redis-host"
+PORT = "redis-port"
+PASSWORD = "redis-password"
+SALT = "redis-salt"
 IP = "0.0.0.0"
 conn = redis.Redis(host=HOST, password=PASSWORD, port=PORT, retry_on_timeout=10)
 TIME = math.floor(time.time())
+TIMEOUT = 259200
+
+
+def get_network():
+    network = {'RX': {
+        'bytes': psutil.net_io_counters().bytes_recv,
+        'packets': psutil.net_io_counters().packets_recv,
+    }, 'TX': {
+        'bytes': psutil.net_io_counters().bytes_sent,
+        'packets': psutil.net_io_counters().packets_sent,
+    }}
+
+    if conn.exists("system_monitor:collection:network:tmp:" + IP):
+        net0 = json.loads(conn.get("system_monitor:collection:network:tmp:" + IP))
+        if network['RX']['packets'] > net0['RX']['packets'] and network['RX']['bytes'] > net0['RX']['bytes'] and \
+                network['TX']['packets'] > net0['TX']['packets'] and network['TX']['bytes'] > net0['TX']['bytes']:
+            conn.zadd("system_monitor:collection:network:RX:" + IP,
+                      {json.dumps(
+                          {"time": TIME, "value": '{},{}'.format(network['RX']['packets'] - net0['RX']['packets'],
+                                                                 network['RX']['bytes'] - net0['RX']['bytes'])}): TIME})
+            conn.zadd("system_monitor:collection:network:TX:" + IP,
+                      {json.dumps(
+                          {"time": TIME, "value": '{},{}'.format(network['TX']['packets'] - net0['TX']['packets'],
+                                                                 network['TX']['bytes'] - net0['TX']['bytes'])}): TIME})
+
+    conn.set("system_monitor:collection:network:tmp:" + IP,
+             json.dumps(network), 86400)
+
+    return network
 
 
 def get_process_num():
     # n-2 '''
-    p = subprocess.Popen("ps aux | wc -l",shell=True,stdout=subprocess.PIPE)
+    p = subprocess.Popen("ps aux | wc -l", shell=True, stdout=subprocess.PIPE)
     return p.stdout.readline().strip()
 
 
 def get_cpu_info():
     # core modelname mhz'''
-    p = subprocess.Popen("cat /proc/cpuinfo | grep name | cut -f2 -d: |uniq",shell=True,stdout=subprocess.PIPE)
+    p = subprocess.Popen("cat /proc/cpuinfo | egrep 'Processor|name' | cut -f2 -d: |uniq", shell=True,
+                         stdout=subprocess.PIPE)
     return p.stdout.readline().strip()
 
 
 def get_cpu_core():
     # core modelname mhz'''
-    p = subprocess.Popen("cat /proc/cpuinfo |grep cores|wc -l",shell=True,stdout=subprocess.PIPE)
-    return p.stdout.readline().strip()
+    return str(psutil.cpu_count())
 
 
-def get_cpu_frequency():
+def get_cpu_temp():
     # core modelname mhz'''
-    p = subprocess.Popen("cat /proc/cpuinfo |grep MHz|cut -f2 -d: |uniq",shell=True,stdout=subprocess.PIPE)
-    return p.stdout.readline().strip()
+    p = subprocess.Popen("cat /sys/devices/virtual/thermal/thermal_zone0/temp", shell=True, stdout=subprocess.PIPE)
+    try:
+        tmp = p.stdout.readline().strip()
+        conn.zadd("system_monitor:collection:thermal:" + IP,
+                  {json.dumps({"time": TIME, "value": str(int(tmp) * 1.0 / 1000)}): TIME})
+        return tmp
+    except:
+        return 0
 
 
 def get_mem_info():
     global IP, TIME
-    # Mem/Swap'''
-    p = subprocess.Popen("free -m|grep -E '(Mem|Swap)'|awk -F' ' '{print $2,$3,$4}'",shell=True,stdout=subprocess.PIPE)
-    info = {}
-    # Mem
-    tmp = p.stdout.readline().strip().split()
-    info['Mem'] = {
-        'total': tmp[0],
-        'used': tmp[1],
-        'free': tmp[2]
-    }
-    
-    conn.zadd("system_monitor:collection:memory:"+IP,{json.dumps({"time": TIME, "value": str(tmp[1])}):TIME})
-    # Swap
-    tmp = p.stdout.readline().strip().split()
-    info['Swap'] = {
-        'total': tmp[0],
-        'used': tmp[1],
-        'free': tmp[2]
-    }
-    conn.zadd("system_monitor:collection:swap:"+IP,{json.dumps({"time": TIME, "value": str(tmp[1])}):TIME})
+    info = {'Mem': {
+        'total': '%.2f' % (psutil.virtual_memory().total / 1024 / 1024),
+        'used': '%.2f' % (psutil.virtual_memory().used / 1024 / 1024),
+        'free': '%.2f' % (psutil.virtual_memory().free / 1024 / 1024),
+    }, 'Swap': {
+        'total': '%.2f' % (psutil.swap_memory().total / 1024 / 1024),
+        'used': '%.2f' % (psutil.swap_memory().used / 1024 / 1024),
+        'free': '%.2f' % (psutil.swap_memory().free / 1024 / 1024)
+    }}
+
+    conn.zadd("system_monitor:collection:memory:" + IP,
+              {json.dumps({"time": TIME, "value": str('%.2f' % (psutil.virtual_memory().used / 1024 / 1024))}): TIME})
+
+    conn.zadd("system_monitor:collection:swap:" + IP,
+              {json.dumps({"time": TIME, "value": str('%.2f' % (psutil.swap_memory().used / 1024 / 1024))}): TIME})
     return info
 
 
 def get_sys_version():
     # System and version'''
-    p = subprocess.Popen("cat /etc/redhat-release",shell=True,stdout=subprocess.PIPE)
+    p = subprocess.Popen(". /usr/lib/os-release;echo $PRETTY_NAME", shell=True, stdout=subprocess.PIPE)
 
     return p.stdout.readline().strip()
 
@@ -68,7 +106,8 @@ def get_sys_version():
 def get_disk_info():
     global IP, TIME
     # disk: total, usage, free, %'''
-    p = subprocess.Popen("df -h --total /|grep total|awk -F' ' '{print $2,$3,$4,$5}'",shell=True,stdout=subprocess.PIPE)
+    p = subprocess.Popen("df -h --total /|grep total|awk -F' ' '{print $2,$3,$4,$5}'", shell=True,
+                         stdout=subprocess.PIPE)
     tmp = p.stdout.readline().strip().split()
     info = {
         'total': tmp[0],
@@ -76,51 +115,72 @@ def get_disk_info():
         'free': tmp[2],
         'percent': tmp[3]
     }
-    conn.zadd("system_monitor:collection:disk:"+IP,{json.dumps({"time": TIME, "value": str(tmp[1])}):TIME})
+    conn.zadd("system_monitor:collection:disk:" + IP,
+              {json.dumps({"time": TIME, "value": str(tmp[1])}): TIME})
     return info
 
 
 def get_ipv4():
     # interface ipv4'''
-    p = subprocess.Popen("ip addr show scope global|grep inet\ |awk -F' ' '{print $2}'",shell=True,stdout=subprocess.PIPE)
-    return stdout_trim(p.stdout.readlines())
+    i = 5
+    while i > 0:
+        try:
+            resp = requests.get(url="http://v4.ipv6-test.com/api/myip.php", timeout=5)
+            if resp.status_code == 200:
+                return resp.text
+        except:
+            i = i - 1
+
+    return "None"
 
 
 def get_ipv6():
     # interface ipv6
-    p = subprocess.Popen("ip addr show scope global|grep inet6|awk -F' ' '{print $2}'",shell=True,stdout=subprocess.PIPE)
-    return stdout_trim(p.stdout.readlines())
+    i = 5
+    while i > 0:
+        try:
+            resp = requests.get(url="http://v6.ipv6-test.com/api/myip.php", timeout=5)
+            if resp.status_code == 200:
+                return resp.text
+        except:
+            i = i - 1
+
+    return "None"
 
 
 def get_connections():
     # establish
-    p = subprocess.Popen("netstat -na|grep ESTABLISHED|wc -l",shell=True,stdout=subprocess.PIPE)
+    p = subprocess.Popen("netstat -na|grep ESTABLISHED|wc -l", shell=True, stdout=subprocess.PIPE)
     return p.stdout.readline().strip()
 
 
 def get_uptime():
     # uptime second
-    p = subprocess.Popen("cat /proc/uptime|awk -F' ' '{print $1}'",shell=True,stdout=subprocess.PIPE)
+    p = subprocess.Popen("cat /proc/uptime|awk -F' ' '{print $1}'", shell=True, stdout=subprocess.PIPE)
     return p.stdout.readline().strip()
+
 
 def get_cpu_load():
     global IP, TIME
     # uptime second
-    p = subprocess.Popen("uptime|awk -F'load average:' '{print $2}'",shell=True,stdout=subprocess.PIPE)
-    tmp = p.stdout.readline().strip().replace(' ','').split(",")
+    p = subprocess.Popen("uptime|awk -F'load average:' '{print $2}'", shell=True, stdout=subprocess.PIPE)
+    tmp = p.stdout.readline().strip().replace(' ', '').split(",")
     load = {
         '1min': tmp[0],
         '5min': tmp[1],
         '15min': tmp[2]
     }
-    conn.zadd("system_monitor:collection:cpu:"+IP,{json.dumps({"time": TIME, "value": str(tmp[1])}):TIME})
+    conn.zadd("system_monitor:collection:cpu:" + IP,
+              {json.dumps({"time": TIME, "value": str(psutil.cpu_percent())}): TIME})
     return load
+
 
 def stdout_trim(so):
     for i in range(0, len(so)):
         so[i] = so[i].strip()
     return so
-    
+
+
 def get_aggragate_json():
     info = {
         'Connection': get_connections(),
@@ -128,38 +188,83 @@ def get_aggragate_json():
         'Uptime': get_uptime(),
         'Memory': get_mem_info(),
         'Load': get_cpu_load(),
-        'Process': get_process_num()
+        'Process': get_process_num(),
+        'Network': get_network(),
+        'Thermal': get_cpu_temp()
     }
     return json.dumps(info)
 
 
 def report():
-    conn.set("system_monitor:stat:"+IP, get_aggragate_json())
-    
+    conn.set("system_monitor:stat:" + IP, get_aggragate_json())
+
+
+def delete_timeout():
+    with conn.pipeline(transaction=False) as p:
+        for k, v in conn.hgetall("system_monitor:hashes").items():
+            hashes = bytes.decode(k)
+            ip = bytes.decode(v)
+            if TIME - float(conn.hmget("system_monitor:info:" + ip, 'Update Time')[0]) > TIMEOUT:
+                p.srem("system_monitor:nodes", ip)
+                p.hdel("system_monitor:hashes", hashes)
+                p.delete("system_monitor:info:" + ip)
+                p.zremrangebyscore("system_monitor:collection:cpu:" + ip, 0, TIME)
+                p.zremrangebyscore("system_monitor:collection:disk:" + ip, 0, TIME)
+                p.zremrangebyscore("system_monitor:collection:memory:" + ip, 0, TIME)
+                p.zremrangebyscore("system_monitor:collection:swap:" + ip, 0, TIME)
+                p.zremrangebyscore("system_monitor:collection:network:RX:" + ip, 0, TIME)
+                p.zremrangebyscore("system_monitor:collection:network:TX:" + ip, 0, TIME)
+                p.delete("system_monitor:stat:" + ip)
+                p.execute()
+
 
 def report_once():
-    '''ip'''
+    """ip"""
     global IP, TIME
-    ip = get_ipv4()
-    if len(ip) > 0:
-        IP = ip[0]
-        
+    IP = get_ipv4()
+
     info = {
-        "CPU":get_cpu_core()+"x "+get_cpu_info()+" @"+get_cpu_frequency()+"MHz",
+        "CPU": get_cpu_core() + "x " + get_cpu_info(),
         "System Version": get_sys_version(),
-        "IPV4": re.sub("\.[0-9]*",".*", ",".join(get_ipv4())),
-        "IPV6": re.sub(":[a-zA-Z0-9]*",":*", ",".join(get_ipv6())),
+        "IPV4": re.sub("[0-9]*\.[0-9]*\.[0-9]*", "*.*.*", get_ipv4()),
+        "IPV6": re.sub("[a-zA-Z0-9]*:", "*:", get_ipv6()),
         "Update Time": TIME
     }
-    
-    conn.sadd("system_monitor:nodes",IP)
-    conn.hmset("system_monitor:hashes",{hashlib.sha256(IP+SALT).hexdigest(): IP})
-    conn.hmset("system_monitor:info:"+IP,info)
-    conn.zremrangebyscore("system_monitor:collection:cpu:"+IP,0,TIME-86400)
-    conn.zremrangebyscore("system_monitor:collection:disk:"+IP,0,TIME-86400)
-    conn.zremrangebyscore("system_monitor:collection:memory:"+IP,0,TIME-86400)
-    conn.zremrangebyscore("system_monitor:collection:swap:"+IP,0,TIME-86400)
+    with conn.pipeline(transaction=False) as pipeline:
+        pipeline.sadd("system_monitor:nodes", IP)
+        pipeline.hmset("system_monitor:hashes", {hashlib.sha256(IP + SALT).hexdigest(): IP})
+        pipeline.hmset("system_monitor:info:" + IP, info)
+        pipeline.zremrangebyscore("system_monitor:collection:cpu:" + IP, 0, TIME - 86400)
+        pipeline.zremrangebyscore("system_monitor:collection:thermal:" + IP, 0, TIME - 86400)
+        pipeline.zremrangebyscore("system_monitor:collection:disk:" + IP, 0, TIME - 86400)
+        pipeline.zremrangebyscore("system_monitor:collection:memory:" + IP, 0, TIME - 86400)
+        pipeline.zremrangebyscore("system_monitor:collection:swap:" + IP, 0, TIME - 86400)
+        pipeline.zremrangebyscore("system_monitor:collection:network:RX:" + IP, 0, TIME - 86400)
+        pipeline.zremrangebyscore("system_monitor:collection:network:TX:" + IP, 0, TIME - 86400)
+        pipeline.execute()
+
     report()
+    delete_timeout()
 
 
+'''
+print("running processes")
+print(get_process_num())
+print("\ncpu info")
+print(get_cpu_info())
+print(get_cpu_core())
+print(get_cpu_temp())
+print("\nip")
+print(get_ipv4())
+print(get_ipv6())
+print("\nconnection")
+print(get_connections())
+print("\nuptime")
+print(get_uptime())
+print("\nsystem version")
+print(get_sys_version())
+print("\nMem & Disk")
+print(get_mem_info())
+print(get_disk_info())
+'''
 report_once()
