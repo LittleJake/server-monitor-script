ace1cce8f0b33764f79f7128bd915acbae4fe037
Jake Liu <13583702+LittleJake@users.noreply.github.com>
2024-11-07 02:01:16 +0800

    Update report.py

--- report.py
+++ report.py
@@ -265,12 +265,13 @@ def get_ipv4():
     # interface ipv4
     global IPV4
     if IPV4 is None:
-        resp = get_request(IPV4_API)
-        if resp is not None and re.match("[0-9]*\\.[0-9]*\\.[0-9]*",resp.text) is not None:
-            IPV4 = resp.text
-        else:
-            IPV4 = "None"
-    
+        try:
+            resp = get_request(IPV4_API)
+            if resp is not None and re.match("[0-9]*\\.[0-9]*\\.[0-9]*",resp.text) is not None:
+                IPV4 = resp.text
+            else:
+                IPV4 = "None"
+        except: IPV4 = "None"
     return IPV4
 
 
@@ -278,11 +279,13 @@ def get_ipv6():
     # interface ipv6
     global IPV6
     if IPV6 is None:
-        resp = get_request(IPV6_API)
-        if resp is not None and re.match("[a-zA-Z0-9]*:",resp.text) is not None:
-            IPV6 = resp.text
-        else:
-            IPV6 = "None"
+        try:
+            resp = get_request(IPV6_API)
+            if resp is not None and re.match("[a-zA-Z0-9]*:",resp.text) is not None:
+                IPV6 = resp.text
+            else:
+                IPV6 = "None"
+        except: IPV6 = "None"
     
     return IPV6
 
